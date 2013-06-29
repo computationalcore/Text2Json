254b11c4f0b6fecd6bd907d5d847b5287286e2d8
Vinícius <computationalcore@gmail.com>
2013-06-29 14:26:35 +0000

    Fix running version

--- text_to_json.go
+++ text_to_json.go
@@ -1,3 +1,6 @@
+/*
+Parse lines of a text file separated with line breakers (\n) converting them to array elements of a json file.
+*/
 package main
 
 import (
@@ -11,32 +14,42 @@ import (
 
 func main() {
 	var (
+		verbose         bool
 		input_file, output_file  string
 	)
 
+	// Command line arguments parser
+	flag.BoolVar(&verbose, "verbose", false, "Print each line from input file while parsing it.")
 	flag.StringVar(&input_file, "input", "input.txt", "Name/Path of the input file  (default is input.txt).")
 	flag.StringVar(&output_file, "output", "output.json", "Name/Path of the output json file (default is output.json).")
+	flag.Parse()
 
 	txtContent := make(map[string]map[string]string)
-	f, _ := os.Open(input_file)
-	r := bufio.NewReader(f)
-	var currentPage map[string]string
-	pageNum := 0
+	file, _ := os.Open(input_file)
+	reader := bufio.NewReader(file)
 	for {
-		line, err := r.ReadString('\n')
+		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err != io.EOF {
 				log.Println("Error in parsing :", err)
 			}
 			break
 		}
+		// If verbose is set print each line while parsing it
+		if verbose {
+			log.Println(line)
+		}
+
 	}
-	f, err := os.Create(output_file)
+	file, err := os.Create(output_file)
 	if err != nil {
 		log.Println("Error :", err)
 		return
 	}
-	defer f.Close()
-	bout, _ := json.Marshal(txtContent)
-	f.Write(bout)
+	defer file.Close()
+	jsonStr, _ := json.Marshal(txtContent)
+	file.Write(jsonStr)
+
+	log.Println(output_file, "file created with success")
+
 }
